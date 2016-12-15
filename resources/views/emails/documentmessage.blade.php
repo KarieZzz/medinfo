aa042130d03d9e156b92f1141479a4bfd8d4283b
Anatoly38 <shameev38@gmail.com>
2016-12-15 01:05:43 +0000

    Исправлены шаблоны сообщений

--- resources/views/emails/documentmessage.blade.php
+++ resources/views/emails/documentmessage.blade.php
@@ -1,4 +1,4 @@
 Пользователь {{ $worker->description }} оставил сообщение:
 <p> {{ $remark }} </p>
-<p>Для документа <a href="http://newmedinfo.miac-io.ru/datainput/formdashboard/{{ $document->id }}">№{{ $document->id }}</a> по форме №{{ $form->form_code }}</p>
+<p>Для документа <a href="http://medinfo.miac-io.ru/datainput/formdashboard/{{ $document->id }}">№{{ $document->id }}</a> по форме №{{ $form->form_code }}</p>
 <p>Учреждение: {{ $unit->unit_name }}</p>
