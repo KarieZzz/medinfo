afd7202b83574a2ceb0ba676666315e25b401c8b
Anatoly38 <shameev38@gmail.com>
2017-05-24 03:50:02 +0000

    Исправлена страничка с логином

--- resources/views/layout/authlayout.blade.php
+++ resources/views/layout/authlayout.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap 3.3.6 -->
     <link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="{{ asset('/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/adminlte/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- AdminLTE Skins. Choose a skin from the css/skins
     folder instead of downloading all of them to reduce the load. -->
     <link href="{{ asset('/dist/css/skins/_all-skins.min.css') }}" rel="stylesheet" type="text/css" />
