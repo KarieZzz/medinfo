90a8c5cdb1d82111f6d86db74c5d6bab8ebb2389
Anatoly38 <shameev38@gmail.com>
2018-08-11 05:25:04 +0000

    Импорт данных из Медстат НСК

--- app/Http/Controllers/ImportExport/MedstatNskDataImportController.php
+++ app/Http/Controllers/ImportExport/MedstatNskDataImportController.php
@@ -24,14 +24,24 @@ class MedstatNskDataImportController extends Controller
     {
         set_time_limit(360);
         \Storage::put(
-            'medstat_uploads/medstat_nsk_data.dbf',
+            'medstat_uploads/medstat_nsk_data.zip',
             file_get_contents($request->file('medstat_nsk_data')->getRealPath())
         );
-        $dbf_file = storage_path('app/medstat_uploads/medstat_nsk_data.dbf');
+        $zip_file = storage_path('app/medstat_uploads/medstat_nsk_data.zip');
+        $zip = new \ZipArchive();
+        if ($zip->open($zip_file) === TRUE) {
+            $data =  $zip->getFromName('Data.DBF');
+            $zip->close();
+        } else {
+            throw  new \Exception("Не удалось открыть файл архива $zip_file");
+        }
+        \Storage::put('medstat_uploads/data.dbf', $data);
+
+        $dbf_file = storage_path('app/medstat_uploads/data.dbf');
         if (!$db = dbase_open($dbf_file, 2)) {
             new \Exception("Ошибка, не получается открыть базу данных $dbf_file");
         }
-        dbase_pack($db);
+        //dbase_pack($db);
         $numrecords = dbase_numrecords($db);
         \App\MedstatNskData::truncate();
         $insert = 'INSERT INTO public.medstat_nsk_data ( hospital, data, year, "table", "column" , "row" ) VALUES ';
