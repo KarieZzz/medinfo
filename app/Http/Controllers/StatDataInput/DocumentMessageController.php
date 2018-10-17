ae8ac5077dd1504121aac72fc5736fe09dc05db8
Anatoly38 <shameev38@gmail.com>
2018-10-17 08:17:39 +0000

    Добавлен метод для тестирования отправки сообщений

--- app/Http/Controllers/StatDataInput/DocumentMessageController.php
+++ app/Http/Controllers/StatDataInput/DocumentMessageController.php
@@ -84,10 +84,10 @@ class DocumentMessageController extends Controller
     {
         $remark = "Тестовое сообщение";
         $for_mail_body = compact('remark');
-        $emails = [ 'shameev@miac-io.ru', 'shameev38@gmail.com'] ;
+        $emails = [ 'shameev@miac-io.ru', 'im@miacmo.ru'] ;
         try {
             Mail::send('emails.test', $for_mail_body, function ($m) use ($emails) {
-                $m->from('medinfo@miac-io.ru', 'Тестовое email сообщение Мединфо');
+                $m->from('noreply@miacmo.ru', 'Тестовое email сообщение Мединфо');
                 $m->to($emails)->subject('Тестовое сообщение');
             });
         } catch(\Exception $e){
